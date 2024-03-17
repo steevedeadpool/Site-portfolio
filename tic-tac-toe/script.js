0603bb2f11abc36a158d58cc5c7d509a875e2016
E <you@example.com>
2024-03-17 20:22:31 +0700

    da

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -3,17 +3,45 @@
 
 
 let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const combo = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+]
 let player = 1;
 let winner;
 
+
+function checkWin() {
+    let zeroNums = 0;
+    board.forEach((item) => {if (item === 0) zeroNums++})
+    if (zeroNums === 0) winner = 'Ничья'
+
+    let p1, p2, p3
+    for(let indexes of combo) {
+        [p1, p2, p3] = indexes
+        if (board[p1] === 1 && board[p2] === 1 && board[p3] === 1) {
+            winner = 'Победил первый игрок!'
+        }
+        else if (board[p1] === 2 && board[p2] === 2 && board[p3] === 2) {
+            winner = 'Победил второй игрок!'
+        }
+    }
+    if (winner) {
+     render()
+     alert(winner)
+    }
+}
 function handleCellClick(index) {
     let cell = board[index]
     if (cell === 0 && !winner) {
         board[index] = player
         player = player == 1 ? 2 : 1
         console.log(board)
+        checkWin()
         render()
     }
+    
 }
 
 
@@ -37,6 +65,5 @@ function render() {
         board_obj.appendChild(cell_obj)
     })
 }
-
 
 render()
